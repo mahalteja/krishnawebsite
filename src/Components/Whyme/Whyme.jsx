d5b3393b80701c7beaf6492cd4834943ee42cd7f
mahalteja <106660291+mahalteja@users.noreply.github.com>
2023-12-19 19:20:41 +0530

    Update Whyme.jsx

--- src/Components/Whyme/Whyme.jsx
+++ src/Components/Whyme/Whyme.jsx
@@ -11,7 +11,7 @@ const Whyme = () => {
   return (
     <div className="whyme">
       <div className="whyme-heading" data-aos="fade">
-        w
+        why me
       </div>
       <div className="items">
         <div className="item" data-aos="fade-up">
